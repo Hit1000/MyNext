6648ff793bc2366261997f392b0249cdbeb6c2f1
HiT1000 <hiteshsingla209@gmail.com>
2025-09-08 01:20:16 +0530

    nesting routing

--- 1_beginning/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ 1_beginning/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,16 @@
-export default async function ReviewId({params} : {
-    params : Promise < {
-        productId: string,
-        reviewId: string
-    } >;
+export default async function ReviewId({
+  params,
+}: {
+  params: Promise<{
+    productId: string;
+    reviewId: string;
+  }>;
 }) {
-    const {productId, reviewId} = await params;
-    return <div>Review {reviewId}
-        for Product {productId}</div>;
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      Review {reviewId}
+      for Product {productId}
+    </div>
+  );
 }
